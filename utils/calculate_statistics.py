437d790240b7dd34c4b320c83e43f8f92a50e61c
Erfan Saberi <erfansaberiow@gmail.com>
2023-12-27 18:14:47 +0330

    Implemented average response time calculator

--- utils/calculate_statistics.py
+++ utils/calculate_statistics.py
@@ -26,11 +26,8 @@ def calculate_statistics(processes: list[Process]) -> Statistics:
         sum(burst.end_time - burst.start_time for burst in cpu_bursts)
         / cpu_bursts[-1].end_time
     )
-    # Average waiting time = Total waiting time / number of processes
     average_waiting_time = calculate_average_waiting_time(processes)
-    # Average turnaround time = Total turnaround time / number of processes
     average_turnaround_time = calculate_average_turnaround_time(processes)
-    # Average response time = Total response time / number of processes
     average_reponse_time = calculate_average_reponse_time(processes)
 
     return Statistics(
@@ -81,4 +78,16 @@ def calculate_average_turnaround_time(processes: list[Process]) -> float:
 
 
 def calculate_average_reponse_time(processes: list[Process]) -> float:
-    return 0  # TODO: Implement Average Response Time calculator
+    """Calculate average response time for a list of processes.
+
+    Args:
+        processes (list[Process]): List of processes
+
+    Returns:
+        float: Average response time for the list of processes
+    """
+    total_response_time = 0
+    for process in processes:
+        first_burst = process.bursts[0]
+        total_response_time += first_burst.start_time - process.arrival_time
+    return total_response_time / len(processes)
