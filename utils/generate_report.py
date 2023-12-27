a01cf81d23ef55d36204dc8c0ecf0412b9b47100
Erfan Saberi <erfansaberiow@gmail.com>
2023-12-27 22:19:47 +0330

    Labels for reported statistics

--- utils/generate_report.py
+++ utils/generate_report.py
@@ -6,13 +6,22 @@ from utils.calculate_statistics import calculate_statistics
 def generate_report(
     processes: list[Process], statistics: Statistics | None = None
 ) -> str:
+    """Generate report from processes and statistics
+
+    Args:
+        processes (list[Process]): _description_
+        statistics (Statistics | None, optional): _description_. Defaults to None.
+
+    Returns:
+        str: _description_
+    """
     if not statistics:
         statistics = calculate_statistics(processes)
-    report = f"{statistics.throughput}\n"
-    report += f"{statistics.cpu_utilization*100}%\n"
-    report += f"{statistics.average_waiting_time}\n"
-    report += f"{statistics.average_turnaround_time}\n"
-    report += f"{statistics.average_reponse_time}\n"
+    report = f"Throughput: {statistics.throughput}\n"
+    report += f"CPU Utilization: {statistics.cpu_utilization*100}%\n"
+    report += f"Avg Waiting Time: {statistics.average_waiting_time}\n"
+    report += f"Avg Turnaround Time: {statistics.average_turnaround_time}\n"
+    report += f"Avg Response Time: {statistics.average_reponse_time}\n"
     for process in processes:
         log = f"{process.id}"
         for burst in process.bursts:
