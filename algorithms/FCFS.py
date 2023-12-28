1311301dc084c69342d7297c60bd390fc6cf6e57
Erfan Saberi <erfansaberiow@gmail.com>
2023-12-29 01:51:17 +0330

    Fixed FCFS algorithms

--- algorithms/FCFS.py
+++ algorithms/FCFS.py
@@ -5,6 +5,18 @@ from schemas.process import Process, ProcessBurst
 
 
 def schedule(processes: list[Process]) -> list[Process]:
+    """FCFS (First Came First Served)  scheduler  algorithm.
+    This scheduler will execute the  processes  that arrived
+    before anothers first. If there is more than one process
+    with same arrival time, it will execute the process with
+    smaller priority number.
+
+    Args:
+        processes (list[Process]): List of processes to schedule
+
+    Returns:
+        list[Process]: List of scheduled processes
+    """
     queue = deepcopy(processes)
     current_time = 0
     current_process = None
@@ -23,7 +35,19 @@ def schedule(processes: list[Process]) -> list[Process]:
             current_time += 1
             continue
 
+        smallest_arrival_time = min(
+            ready_processes,
+            key=lambda proc: proc.arrival_time,
+        ).arrival_time
+
+        ready_processes = list(
+            filter(
+                lambda proc: proc.arrival_time == smallest_arrival_time,
+                ready_processes,
+            )
+        )
         ready_processes.sort(key=lambda proc: proc.priority)
+
         current_process = ready_processes[0]
         current_process.bursts.append(
             ProcessBurst(
